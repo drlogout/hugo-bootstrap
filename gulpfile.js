92c507d7944ef12a28d2d37967107420be2a1bea
Christian Nolte <hello@noltech.net>
2017-09-23 21:09:27 +0200

    Compile scss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function() {
     return gulp.src([
-            'node_modules/bootstrap/scss/bootstrap.scss',
-            'src/scss/*scss'
+            'src/scss/style.scss'
         ])
         .pipe(sass())
+        .pipe(rename('app.css'))
         .pipe(gulp.dest('static/assets/css'));
-})
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+            'node_modules/bootstrap/js/dist/collapse.js'
+        ])
+        .pipe(gulp.concat('app.js'))
+        .pipe(gulp.dest('static/assets/js'));
+});
